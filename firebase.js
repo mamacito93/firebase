4c47f867fa02df696832b247ef7ba5bd7c389b8a
mamacito93 <vellencunee@gmail.com>
2023-09-29 10:20:54 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "", // TODO
+  authDomain: "", // TODO
+  databaseURL: "", // TODO
+  projectId: "", // TODO
+  storageBucket: "" // TODO
+})
+
+export default app
