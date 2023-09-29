bc5b7b92d8b6509dbed03a140242e37d8f0432cc
mamacito93 <vellencunee@gmail.com>
2023-09-29 10:37:02 +0200

    firebaz

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
 import { initializeApp } from 'firebase/app'
 
 const app = initializeApp({
-  apiKey: "", // TODO
-  authDomain: "", // TODO
-  databaseURL: "", // TODO
-  projectId: "", // TODO
-  storageBucket: "" // TODO
-})
+  apiKey: "AIzaSyAIo8PApQcY8wjwJHFmKxGKVPCMGplBRZQ",
+  authDomain: "mamacito-cloud.firebaseapp.com",
+  projectId: "mamacito-cloud",
+  })
 
 export default app
